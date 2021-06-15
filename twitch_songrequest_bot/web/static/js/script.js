6f9ec91b52242253c57ea1bf40d1d788bdc5809a
albertojnk <albertojanicke@live.com>
2021-06-15 19:16:52 -0300

    :construction: twitch login

--- twitch_songrequest_bot/web/static/js/script.js
+++ twitch_songrequest_bot/web/static/js/script.js
@@ -1,4 +1,23 @@
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("teste").innerText = "aaaaaaaaaaaaaaaaaaaaaaaaaa"
+document.getElementById("btn-login").addEventListener("click", function (e) {
+    backend_url = "http://localhost:5000/twitch/oauth"
+    const http = new XMLHttpRequest();
+    http.open("GET", backend_url);
+    http.send();
+    http.onreadystatechange=(e)=>{
+        window.location.href = "http://localhost:5000"
+    }
+    e.preventDefault()    
 })
+
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
--- twitch_songrequest_bot/web/web.py
+++ twitch_songrequest_bot/web/web.py
@@ -1,3 +1,7 @@
+import requests
+import random
+import string
+import os 
 from flask import Flask, render_template, request
 from twitch_songrequest_bot.models import collection
 
@@ -9,11 +13,30 @@ app.config["DEBUG"] = True
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/twitch/oauth', methods=['post'])
+def twitch_oauth():
+    scopes = "viewing_activity_read"
+    state = get_random_string(32)
+    url = "https://id.twitch.tv/oauth2/authorize?client_id=" + os.environ("TWITCH_CLIENT_ID")
+    url += "&redirect_uri=" + os.environ("TWITCH_REDIRECT_URI")
+    url += "&response_type=code"
+    url += "&scope=" + scopes
+    url += "&state=" + state
+    return requests.get(url).content
+
 
 @app.route('/api/playlist', methods=['post'])
 def playlist():
     link = request.form['link']
     collection.update()
     return f"{link}"
+
+def get_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
 app.run()
